123d7d6146fcb8c9533b5d786be546d0f8bee8bd
kponceg <kponcegu@uci.edu>
2026-08-18 21:18:09 -0700

    Restructured the layout again, added vite server to frontend.

--- server/src/app.js
+++ server/src/app.js
@@ -12,12 +12,10 @@ app.use(
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({
-        message: "Nail tech API is running",
-    });
+    res.json({message: "Nail tech API is running"});
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
